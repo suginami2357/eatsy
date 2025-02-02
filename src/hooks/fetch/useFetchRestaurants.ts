b1f0fb18031d7b50520ee2b8f91f804df8f582f1
oishi <suginami2357@gmail.com>
2025-02-02 20:05:23 +0900

    useFetchRestaurantsリファクタリング

--- src/hooks/fetch/useFetchRestaurants.ts
+++ src/hooks/fetch/useFetchRestaurants.ts
@@ -1,7 +1,7 @@
 import useSWRInfinite from "swr/infinite";
 import type { Restaurant } from "~/types/restaurant";
 
-type options = {
+type Options = {
 	setHasMore: React.Dispatch<React.SetStateAction<boolean>>;
 	pageSize: number;
 	position: GeolocationPosition | undefined;
@@ -11,17 +11,17 @@ export const useFetchRestaurants = ({
 	setHasMore,
 	pageSize,
 	position,
-}: options) => {
+}: Options) => {
 	const getKey = (pageIndex: number, previousPageData: Restaurant | null) => {
 		if (previousPageData && !previousPageData.results.shop.length) {
 			setHasMore(false);
 			return null;
 		}
-		const query = position
+		const params = position
 			? `lat=${position.coords.latitude}&lng=${position.coords.longitude}&range=5`
 			: "large_area=Z011";
 
-		return `/api/restaurants?${query}&start=${pageIndex * pageSize + 1}&count=${pageSize}`;
+		return `/api/restaurants?${params}&start=${pageIndex * pageSize + 1}&count=${pageSize}`;
 	};
 
 	const fetcher = (url: string): Promise<Restaurant> =>
