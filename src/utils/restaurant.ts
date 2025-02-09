05b9b4a45c034102f4d10f7aec68f84f4863dc2c
oishi <suginami2357@gmail.com>
2025-02-09 20:49:32 +0900

    formatDataリファクタリング

--- src/utils/restaurant.ts
+++ src/utils/restaurant.ts
@@ -4,35 +4,102 @@ import type { Restaurant } from "~/types/restaurant";
 export function formatData(restaurants: Restaurant[] | undefined) {
 	if (!restaurants) return [];
 
-	const data = restaurants?.flatMap((x) => x.results.shop);
-
-	const result = data
+	return restaurants
+		.flatMap((x) => x.results.shop)
 		.map((item) => {
 			return {
 				...item,
-				genre: {
-					code: item.genre.code,
-					name: item.genre.name
-						.split("・")
-						.reduce((a, b) => (a.length > b.length ? a : b)),
-					catch: item.genre.catch,
-				},
+				genre: formatGenre(item.genre),
 				card: formatCard(item.card),
-				station_name: `${item.station_name}駅`,
-				budget: {
-					code: item.budget.code,
-					name: `～${Number(
-						item.budget.name.replace("円", "").split("～")?.[1],
-					).toLocaleString()}円`,
-					average: Number(item.budget.average),
-				},
-				mobile_access: formatAccess(item.mobile_access),
-				open: item.open.replace(/\（.*?\）/g, ""),
+				station_name: formatStationName(item.station_name),
+				budget: formatBudget(item.budget),
+				mobile_access: formatMobileAccess(item.mobile_access),
+				open: formatOpen(item.open),
 				non_smoking: formatNonSmoking(item.non_smoking),
 			};
 		})
 		.filter((x) => Object.values(x).every((value) => value !== undefined));
+}
+
+function formatGenre(value: {
+	code: string;
+	name: string;
+	catch: string;
+}) {
+	return {
+		code: value.code,
+		name: value.name
+			.split("・")
+			.reduce((a, b) => (a.length > b.length ? a : b)),
+		catch: value.catch,
+	};
+}
+
+function formatStationName(value: string): string {
+	return `${value}駅`;
+}
+
+function formatBudget(value: {
+	code: string;
+	name: string;
+	average: string;
+}) {
+	return {
+		code: value.code,
+		name: `～${Number(
+			value.name.replace("円", "").split("～")?.[1],
+		).toLocaleString()}円`,
+		average: Number(value.average),
+	};
+}
+
+function formatMobileAccess(value: string): string | undefined {
+	let result = value;
+
+	// キーワードが含まれて無ければ "-" を返す
+	if (!["駅", "徒歩"].every((keyword) => result.includes(keyword))) {
+		return undefined;
+	}
+
+	// ◆♪,☆★●◎!※で分割してキーワードが無ければ削除
+	result = result
+		.split(/◆|♪|,|☆|★|●|◎|!|※|｡/)
+		.filter((part) => ["JR", "線", "駅", "徒歩"].some((x) => part.includes(x)))
+		.join("");
+
+	// 【】《》『』がペアでない場合は削除
+	const pairs = [
+		{ open: "【", close: "】" },
+		{ open: "《", close: "》" },
+		{ open: "『", close: "』" },
+	];
+	for (const pair of pairs) {
+		const openMatch = result.match(new RegExp(`\\${pair.open}`, "g"));
+		const closeMatch = result.match(new RegExp(`\\${pair.close}`, "g"));
+		if (openMatch?.length !== closeMatch?.length) {
+			result = result.replaceAll(pair.open, "").replaceAll(pair.close, "");
+		}
+	}
+
+	// 【】《》『』 中に "駅" が無ければ削除
+	result = result.replace(/【[^】]*】|《[^》]*》|『[^』]*』/g, (match) =>
+		match.includes("駅") ? match : "",
+	);
+
+	// 【】《》『』を削除
+	result = result.replace(/[【】《》『』]/g, "");
+
+	// "," →　"/" に変換
+	result = result.replace(/,/g, "/");
+
+	// 一番最後の "分" "秒" より後方を削除
+	result = result.replace(/(分|秒)[^分秒]*$/, "$1");
+
 	return result;
+}
+
+function formatOpen(value: string): string {
+	return value.replace(/\（.*?\）/g, "");
 }
 
 function formatCard(value: string): string | undefined {
@@ -57,50 +124,6 @@ function formatNonSmoking(value: string): string | undefined {
 		default:
 			return undefined;
 	}
-}
-
-function formatAccess(value: string): string | undefined {
-	let result = value;
-
-	// キーワードが含まれて無ければ "-" を返す
-	if (!["駅", "徒歩"].some((keyword) => result.includes(keyword))) {
-		return undefined;
-	}
-
-	// "," →　"/" に変換
-	result = result.replace(/,/g, "/");
-
-	// 【】《》『』 中に "駅" が無ければ削除
-	result = result.replace(/【[^】]*】|《[^》]*》|『[^』]*』/g, (match) =>
-		match.includes("駅") ? match : "",
-	);
-
-	// ◆♪,☆★●◎!※で分割してキーワードが無ければ削除
-	result = result
-		.split(/◆|♪|,|☆|★|●|◎|!|※|｡/)
-		.filter((part) =>
-			["駅", "徒歩", "分", "秒", "m", "階", "F"].some((x) => part.includes(x)),
-		)
-		.join("");
-
-	// 一番最後の"分","秒"より後ろを削除
-	result = result.replace(/(分|秒)[^分秒]*$/, "$1");
-
-	// 【】《》『』がペアでない場合は削除
-	const pairs = [
-		{ open: "【", close: "】" },
-		{ open: "《", close: "》" },
-		{ open: "『", close: "』" },
-	];
-	for (const pair of pairs) {
-		const openMatch = result.match(new RegExp(`\\${pair.open}`, "g"));
-		const closeMatch = result.match(new RegExp(`\\${pair.close}`, "g"));
-		if (openMatch?.length !== closeMatch?.length) {
-			result = result.replaceAll(pair.open, "").replaceAll(pair.close, "");
-		}
-	}
-
-	return result;
 }
 
 export function formatDistance(
