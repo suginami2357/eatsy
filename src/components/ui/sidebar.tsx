3b7b061d6e4cdfa4a93cbe5b4a5f1b28b8d98e3c
oishi <suginami2357@gmail.com>
2025-02-16 16:46:35 +0900

    sadebar修正

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -1,18 +1,38 @@
 import clsx from "clsx";
+import dynamic from "next/dynamic";
 
 type SidebarProps = {
+	isMobile: boolean;
 	isOpen: boolean;
+	setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
 } & React.PropsWithChildren;
 
-export default function Sidebar({ isOpen, children }: SidebarProps) {
+function Sidebar({ isMobile, isOpen, setIsOpen, children }: SidebarProps) {
 	return (
 		<div
 			className={clsx(
-				"flex fixed inset-0 w-full h-full z-20 transform transition-transform duration-300 ease-in-out",
+				"flex fixed inset-0 z-20 transform transition-transform duration-300 ease-in-out",
 				isOpen ? "translate-x-0" : "-translate-x-full",
 			)}
 		>
-			{children}
+			{/* オーバーレイ */}
+			{isMobile && (
+				<button
+					className={clsx(
+						"flex fixed inset-0 bg-black bg-opacity-50 transition-opacity",
+						isOpen ? "opacity-100 delay-[150ms]" : "opacity-0",
+					)}
+					onClick={() => setIsOpen(false)}
+					type="button"
+				/>
+			)}
+
+			{/* サイドバー */}
+			<div className="flex z-30 w-80 h-full bg-white shadow-lg">
+				<div className="text-gray-900">{children}</div>
+			</div>
 		</div>
 	);
 }
+
+export default dynamic(() => Promise.resolve(Sidebar), { ssr: false });
